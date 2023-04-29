a6311d74afcf1fb2b34d0ae4f1487318cd8bfc59
unknown <ctrhinkle@gmail.com>
2023-04-29 21:27:14 +0200

    remove slash for deploy

--- data/ItemData.jsx
+++ data/ItemData.jsx
@@ -4,7 +4,7 @@ const itemData = [
     {
       id: 1,
       name: "Nature's Orchestra",
-      image: "/assets/Pack_Art/tmpKontakt.png",           // WHEN DEPLOYING (todo) simple remove the slash before assets
+      image: "assets/Pack_Art/tmpKontakt.png",           // WHEN DEPLOYING (todo) simple remove the slash before assets
       price: 50.00,
       path: "/products/Natures_Orchestra"
     },
@@ -14,14 +14,14 @@ const itemData = [
     {
       id: 2,
       name: "Vivid Percussion 1",
-      image: "/assets/Pack_Art/VP01_Art.png",
+      image: "assets/Pack_Art/VP01_Art.png",
       price: 10.00,
       path: "/products/Vivid_Percussion_01"
     },
     {
       id: 3,
       name: "Forest Textures 1",
-      image: "/assets/Pack_Art/Forest_Textures_01_ART_Square.png",
+      image: "assets/Pack_Art/Forest_Textures_01_ART_Square.png",
       price: 15.00,
       path: "/products/Forest_Textures_01"
     },
@@ -31,14 +31,14 @@ const itemData = [
     {
       id: 2,
       name: "Sturdy Tables 1",
-      image: "/assets/Pack_Art/ST01_Inkhazy_-_Art.png",
+      image: "assets/Pack_Art/ST01_Inkhazy_-_Art.png",
       price: 5.00,
       path: "/products/Sturdy_Tables_01"
     },
     {
       id: 3,
       name: "Sturdy Tables 2",
-      image: "/assets/Pack_Art/ST02_Inkhazy_-_Art.jpg",
+      image: "assets/Pack_Art/ST02_Inkhazy_-_Art.jpg",
       price: 8.00,
       path: "/products/Sturdy_Tables_02"
     },
