fd57cefd411338423623e4a4801bac903013850a
unknown <ctrhinkle@gmail.com>
2023-05-03 20:18:05 +0200

    fix width issue

--- components/ProductInfo.jsx
+++ components/ProductInfo.jsx
@@ -5,22 +5,16 @@ const ProductInfo = ({rowIDX, colIDX, backgroundDim}) =>
     console.log(backgroundDim)
     return (
     <div className={`${backgroundDim} w-full h-[2600px] flex-auto leading-[70px]`} >
-        <div className="text-white relative top-24 left-[0px] width w-[full] text-left">
+        <div className="text-white relative top-24 left-[0px] w-[full] text-left">
             <h1>{itemData[rowIDX][colIDX].title}</h1>
             <div className='h-[24px]'></div>
-            <pre>
                 <p className='text-[28px]'>{itemData[rowIDX][colIDX].description}</p>
-            </pre>
             <h1>{itemData[rowIDX][colIDX].titleJP}</h1>
             <div className='h-[24px]'></div>
-            <pre>
                 <p className='text-[28px]'>{itemData[rowIDX][colIDX].descriptionJP}</p>
-            </pre>
 
             <h1>Extra Info:</h1>
-            <pre>
                 <p className='text-[28px]'>{itemData[rowIDX][colIDX].extraInfo}</p>
-            </pre>
 
             <h1>Artists from the demo songs:</h1>
             <pre>
